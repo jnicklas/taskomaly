537199bd1aa0b424d1dd06e06f07434d7723f6f8
Ardekantur <greystone@ardekantur.com>
2008-07-08 23:11:03 -0400

    need yaml, + some specs filled out

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 module Taskomaly
   
   class API    
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,16 +2,37 @@ require File.dirname(__FILE__) + '/base'
 
 describe "Taskomaly::API" do
 
-  it "requires a user ID" do
-    pending "Not done yet"
+  API_CONFIG_LOCATION = File.join(File.dirname(__FILE__), 'fixtures', 'api_config.yml')
+  API_CONFIG_USER = 8125
+  API_CONFIG_KEY  = 'de18208d17dcc2aa007574fdb7c26322'
+  
+  LOCAL_TEST_PROJECT  = File.join(File.dirname(__FILE__), 'fixtures', 'test_project_one.taskPaper')
+
+  before do
+    
+    opts = { 'user' => API_CONFIG_USER, 'key' => API_CONFIG_KEY }
+    File.open( API_CONFIG_LOCATION, 'w' ) do |out|
+        YAML.dump( opts, out )
+      end
   end
 
-  it "requires an API key" do
-    pending "Not done yet"
+  it "requires both a user ID and an API key, or the location of a configuration file" do
+    lambda { bad_t = Taskomaly::API.new }.should raise_error( ArgumentError )
+    lambda { bad_t = Taskomaly::API.new :user => 0 }.should raise_error( ArgumentError )
+    lambda { bad_t = Taskomaly::API.new :key => 'mumbo-jumbo' }.should raise_error( ArgumentError )
+    
+    t = Taskomaly::API.new :user => 9999, :key => 'mumbo-jumbo'
+    t.user.should == 9999
+    t.key.should == 'mumbo-jumbo'
+    
+    lambda { config_t = Taskomaly::API.new :config => 'dummy_file.yml' }.should_not raise_error( ArgumentError )
   end
 
   it "can load a user ID and API key from a configuration file" do
-    pending "Not done yet"
+    t = Taskomaly::API.new :config => API_CONFIG_LOCATION
+    t.user.should == API_CONFIG_USER
+    t.key.should  == API_CONFIG_KEY
+    
   end
 
   it "needs a place to store a response from the server" do
